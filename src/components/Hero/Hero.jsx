9100d23108aee7f8efb1081c7fc573d0c69aacef
Jonathan Guidi <jguidi.info@gmail.com>
2023-06-22 14:50:23 -0300

    fix link wsp

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,10 +5,10 @@ const Hero = () => {
   return (
     <section id="hero" className="hero-container">
       <div className="hero-content">
-        <h1 className="hero-title">📍 Tu Hamburguesería de barrio <br/> 🍔🤝🍻</h1>
+        <h1 className="hero-title">📍 Tu Hamburguesería de barrio <br /> 🍔🤝🍻</h1>
         <div className="hero-buttons">
           <button className="btn-menu">
-            <Link to="/App"> 
+            <Link to="/App">
               HACE TU PEDIDO!
             </Link>
           </button>
@@ -17,7 +17,7 @@ const Hero = () => {
             </Link>
           </button>
           <button className="btn-wsp">
-            <Link to="https://web.whatsapp.com/send?phone=5491122529712&text=Holaa,%20%C2%BFC%C3%B3mo%20va?%20Somos%20BR-BURGERS,%20Escribe%20tu%20Pedido%20o%20Consulta%20aqu%C3%AD...%0A%C2%A1Gracias!" target="_blank" rel="noreferrer">
+            <Link to="https://wa.me/5491122529712?text=Holaa,%20%C2%BFC%C3%B3mo%20va?%20Somos%20BR-BURGERS,%20Escribe%20tu%20Pedido%20o%20Consulta%20aqu%C3%AD...%0A%C2%A1Gracias!" target="_blank" rel="noreferrer">
               CONTACTO
             </Link>
           </button>
